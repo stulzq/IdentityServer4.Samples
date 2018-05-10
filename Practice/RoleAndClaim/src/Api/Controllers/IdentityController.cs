d7f726b7143aac62353a6544533287b681836c1c
lizhiqiang <lizhiqiang@geekbuy.com.cn>
2018-05-10 10:59:32 +0800

    add practice demo

---
+++ Practice/RoleAndClaim/src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Controllers
+{
+
+	
+	[Route("[controller]")]
+    
+    public class IdentityController : ControllerBase
+    {
+	    [Authorize(Roles = "superadmin")]
+		[HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in HttpContext.User.Claims select new { c.Type, c.Value });
+        }
+
+	    [Authorize(Roles = "admin")]
+		[Route("{id}")]
+	    [HttpGet]
+	    public string Get(int id)
+	    {
+		    return id.ToString();
+	    }
+	}
+}
---
+++ Practice/RoleAndClaim/src/QuickstartIdentityServer/Config.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
+
+namespace QuickstartIdentityServer
+{
+    public class Config
+    {
+	    public static IEnumerable<IdentityResource> GetIdentityResourceResources()
+	    {
+		    var customProfile = new IdentityResource(
+			    name: "custom.profile",
+			    displayName: "Custom profile",
+			    claimTypes: new[] { "role"});
+
+			return new List<IdentityResource>
+		    {
+			    new IdentityResources.OpenId(), 
+			    new IdentityResources.Profile(),
+			    customProfile
+			};
+	    }
+		// scopes define the API resources in your system
+		public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+//                new ApiResource("api1", "My API")
+                new ApiResource("api1", "My API",new List<string>(){JwtClaimTypes.Role})
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" ,IdentityServerConstants.StandardScopes.OpenId, 
+	                    IdentityServerConstants.StandardScopes.Profile}
+                },
+
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" ,IdentityServerConstants.StandardScopes.OpenId, 
+	                    IdentityServerConstants.StandardScopes.Profile,"custom.profile"}
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+	                Claims = new List<Claim>(){new Claim(JwtClaimTypes.Role,"superadmin") }
+				},
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password",
+					
+					Claims = new List<Claim>(){new Claim(JwtClaimTypes.Role,"admin") },
+						
+                }
+            };
+        }
+    }
+}
