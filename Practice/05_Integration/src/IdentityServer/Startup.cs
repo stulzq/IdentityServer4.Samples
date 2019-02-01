c20b5729da663d7e2fa197896219e2c6e53688b8
stulzq <stulzq@qq.com>
2019-02-01 15:47:00 +0800

    [Add] Demo Integration

---
+++ Practice/05_Integration/src/IdentityServer/Startup.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer(options=>options.Authentication.CookieAuthenticationScheme= "Cookies")
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+
+            services.AddAuthentication("Bearer")
+                .AddCookie("Cookies")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    //identityserver4 地址 也就是本项目地址
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "api1";
+                });
+
+
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to support static files
+            //app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+
+            // uncomment, if you wan to add an MVC-based UI
+            //            app.UseMvcWithDefaultRoute();
+            app.UseMvc();
+        }
+    }
+}
